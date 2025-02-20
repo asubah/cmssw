35d5aff43decb3020615e7ceb488fc1c54e5093c
Abdulla Subah <abdulla.e.subah@gmail.com>
2025-02-20 19:31:17 +0100

    refactored interface

--- HeterogeneousCore/KernelConfigurations/interface/KernelConfigurations.h
+++ HeterogeneousCore/KernelConfigurations/interface/KernelConfigurations.h
@@ -4,148 +4,153 @@
 #include "FWCore/ParameterSet/interface/ParameterSet.h"
 #include "FWCore/ParameterSet/interface/ParameterSetDescription.h"
 
-#include <iostream>
+#include <array>
 #include <regex>
+#include <unordered_map>
+#include <vector>
 
 namespace cms {
 
   struct LaunchConfig {
     std::string kernelName;
-    std::array<uint32_t, 3> threads;
-    std::array<uint32_t, 3> blocks;
+    std::array<uint32_t, 3> threads = {{1, 1, 1}};
+    std::array<uint32_t, 3> blocks = {{1, 1, 1}};
   };
 
   class LaunchConfigs {
   public:
-    void insertConfig(LaunchConfig config) { launchConfigs_.emplace_back(config); };
+    void insertConfig(LaunchConfig config) {
+      configs_.try_emplace(config.kernelName, std::move(config));
+    }
 
-    LaunchConfig getConfig(std::string kernelName) const {
-      for (const auto& config : launchConfigs_) {
-        if (config.kernelName == kernelName) {
-          LaunchConfig wantedConfig;
-          wantedConfig.kernelName = config.kernelName;
-          wantedConfig.threads = config.threads;
-          wantedConfig.blocks = config.blocks;
-
-          return wantedConfig;
-        }
-      }
-
-      return {"", {{0, 0, 0}}, {{0, 0, 0}}};
+    LaunchConfig getConfig(const std::string& kernelName) const {
+      const auto it = configs_.find(kernelName);
+      return it != configs_.end() ? it->second : LaunchConfig{};
     }
 
   private:
-    std::vector<LaunchConfig> launchConfigs_;
+    std::unordered_map<std::string, LaunchConfig> configs_;
   };
 
   class KernelConfigurations {
   public:
-    KernelConfigurations(const edm::ParameterSet& iConfig) {
-      configurations_ = iConfig.getParameter<edm::ParameterSet>("kernels");
-    }
-    
-    void init(const edm::ParameterSet& iConfig) {
-      configurations_ = iConfig.getParameter<edm::ParameterSet>("kernels");
-    }
-
-    LaunchConfigs getConfigsForDevice(std::string device) const {
-      LaunchConfigs wantedConfigs;
-      LaunchConfig config;
-      std::vector<uint32_t> threadsVec;
-      std::vector<uint32_t> blocksVec;
-
-      std::vector<std::string> parametersNames = configurations_.getParameterNames();
-      std::vector<edm::ParameterSet> kernel;
-
+    struct KernelDescription {
+      std::string name;
       std::vector<uint32_t> threads;
       std::vector<uint32_t> blocks;
+      std::vector<uint32_t> minThreads;
+      std::vector<uint32_t> maxThreads;
+      std::vector<uint32_t> minBlocks;
+      std::vector<uint32_t> maxBlocks;
+    };
 
-      for (const auto& name : parametersNames) {
-        kernel = configurations_.getParameter<std::vector<edm::ParameterSet>>(name);
-        for (const auto& p : kernel) {
-          std::string devicePattern = p.getParameter<std::string>("device");
-          if (std::regex_search(device, std::regex(devicePattern))) {
-            config.kernelName = name;
-            threads = p.getParameter<std::vector<uint32_t>>("threads");
-            blocks = p.getParameter<std::vector<uint32_t>>("blocks");
+    explicit KernelConfigurations(const edm::ParameterSet& config)
+        : configurations_(config.getParameter<edm::ParameterSet>("kernels")) {}
 
-            std::copy(threads.begin(), threads.end(), config.threads.begin());
-            std::copy(blocks.begin(), blocks.end(), config.blocks.begin());
-
-            wantedConfigs.insertConfig(config);
+    LaunchConfigs getConfigsForDevice(const std::string& device) const {
+      LaunchConfigs configs;
+      for (const auto& name : configurations_.getParameterNames()) {
+        const auto& params = configurations_.getParameter<std::vector<edm::ParameterSet>>(name);
+        for (const auto& pset : params) {
+          if (deviceMatches(device, pset.getParameter<std::string>("device"))) {
+            configs.insertConfig(parseLaunchConfig(name, pset));
             break;
           }
         }
       }
-
-      return wantedConfigs;
+      return configs;
     }
 
-    static void fillKernelDescriptionsNames(edm::ParameterSetDescription& desc, std::vector<std::string> kernelsNames) {
-      edm::ParameterSetDescription configsDesc;
-      configsDesc.add<std::string>("device", "");
-      configsDesc.add<std::vector<uint32_t>>("threads", {0});
-      configsDesc.add<std::vector<uint32_t>>("blocks", {0});
-
-      std::vector<edm::ParameterSet> defaultConfigsDescVector;
-
-      edm::ParameterSet defaultConfigsDesc;
-      defaultConfigsDesc.addParameter<std::string>("device", "");
-      defaultConfigsDesc.addParameter<std::vector<uint32_t>>("threads", {0});
-      defaultConfigsDesc.addParameter<std::vector<uint32_t>>("blocks", {0});
-
-      defaultConfigsDescVector.push_back(defaultConfigsDesc);
-
+    static void fillBasicDescriptions(edm::ParameterSetDescription& desc,
+                                      const std::vector<std::string>& kernelNames) {
       edm::ParameterSetDescription kernelsDesc;
-
-      for (const auto& name : kernelsNames) {
-        kernelsDesc.addVPSet(name, configsDesc, defaultConfigsDescVector);
+      const auto configDesc = createBasicConfigDescription();
+      
+      for (const auto& name : kernelNames) {
+        kernelsDesc.addVPSet(name, configDesc, {createBasicDefaultConfig()});
       }
-
+      
       desc.add<edm::ParameterSetDescription>("kernels", kernelsDesc);
     }
 
-    static void fillKernelDescriptionsDetails(edm::ParameterSetDescription& desc,
-                                       std::vector<std::tuple<std::string,            // kernel name
-                                                              std::vector<uint32_t>,  // threads
-                                                              std::vector<uint32_t>,  // blocks
-                                                              std::vector<uint32_t>,  // minThreads
-                                                              std::vector<uint32_t>,  // maxThredas
-                                                              std::vector<uint32_t>,  // minBlocks
-                                                              std::vector<uint32_t>   // maxBlocks
-                                                              >> kernelsDescs) {
-      edm::ParameterSetDescription defaults;
-
-      for (const auto& kernelDesc : kernelsDescs) {
-        auto& [kernelName, threads, blocks, minThreads, maxThreads, minBlocks, maxBlocks] = kernelDesc;
-        edm::ParameterSetDescription configsDesc;
-        configsDesc.add<std::string>("device", "");
-        configsDesc.add<std::vector<uint32_t>>("threads", threads);
-        configsDesc.add<std::vector<uint32_t>>("blocks", blocks);
-        configsDesc.add<std::vector<uint32_t>>("minThreads", minThreads);
-        configsDesc.add<std::vector<uint32_t>>("maxThreads", maxThreads);
-        configsDesc.add<std::vector<uint32_t>>("minBlocks", minBlocks);
-        configsDesc.add<std::vector<uint32_t>>("maxBlocks", maxBlocks);
-
-        edm::ParameterSet defaultConfigsDesc;
-        defaultConfigsDesc.addParameter<std::vector<uint32_t>>("threads", threads);
-        defaultConfigsDesc.addParameter<std::vector<uint32_t>>("blocks", blocks);
-        defaultConfigsDesc.addParameter<std::vector<uint32_t>>("minThreads", minThreads);
-        defaultConfigsDesc.addParameter<std::vector<uint32_t>>("maxThreads", maxThreads);
-        defaultConfigsDesc.addParameter<std::vector<uint32_t>>("minBlocks", minBlocks);
-        defaultConfigsDesc.addParameter<std::vector<uint32_t>>("maxBlocks", maxBlocks);
-
-        std::vector<edm::ParameterSet> defaultConfigsDescVector;
-        defaultConfigsDescVector.push_back(defaultConfigsDesc);
-
-        defaults.addVPSet(kernelName, configsDesc, defaultConfigsDescVector);
+    static void fillDetailedDescriptions(edm::ParameterSetDescription& desc,
+                                         const std::vector<KernelDescription>& kernels) {
+      edm::ParameterSetDescription kernelsDesc;
+      
+      for (const auto& kernel : kernels) {
+        kernelsDesc.addVPSet(kernel.name,
+                            createDetailedConfigDescription(kernel),
+                            {createDetailedDefaultConfig(kernel)});
       }
-
-      desc.add<edm::ParameterSetDescription>("kernels", defaults);
+      
+      desc.add<edm::ParameterSetDescription>("kernels", kernelsDesc);
     }
 
   private:
     edm::ParameterSet configurations_;
+
+    static bool deviceMatches(const std::string& device, const std::string& pattern) {
+      try {
+        return std::regex_search(device, std::regex(pattern));
+      } catch (const std::regex_error&) {
+        return false;
+      }
+    }
+
+    static LaunchConfig parseLaunchConfig(const std::string& name, const edm::ParameterSet& pset) {
+      LaunchConfig config;
+      config.kernelName = name;
+      config.threads = convertToDimensions(pset.getParameter<std::vector<uint32_t>>("threads"));
+      config.blocks = convertToDimensions(pset.getParameter<std::vector<uint32_t>>("blocks"));
+      return config;
+    }
+
+    static std::array<uint32_t, 3> convertToDimensions(const std::vector<uint32_t>& input) {
+      std::array<uint32_t, 3> output{{1, 1, 1}};
+      for (size_t i = 0; i < input.size() && i < 3; ++i) {
+        output[i] = input[i];
+      }
+      return output;
+    }
+
+    static edm::ParameterSetDescription createBasicConfigDescription() {
+      edm::ParameterSetDescription desc;
+      desc.add<std::string>("device", "");
+      desc.add<std::vector<uint32_t>>("threads", {1});
+      desc.add<std::vector<uint32_t>>("blocks", {1});
+      return desc;
+    }
+
+    static edm::ParameterSet createBasicDefaultConfig() {
+      edm::ParameterSet pset;
+      pset.addParameter<std::string>("device", "");
+      pset.addParameter<std::vector<uint32_t>>("threads", {1});
+      pset.addParameter<std::vector<uint32_t>>("blocks", {1});
+      return pset;
+    }
+
+    static edm::ParameterSetDescription createDetailedConfigDescription(const KernelDescription& desc) {
+      edm::ParameterSetDescription configDesc;
+      configDesc.add<std::string>("device", "");
+      configDesc.add<std::vector<uint32_t>>("threads", desc.threads);
+      configDesc.add<std::vector<uint32_t>>("blocks", desc.blocks);
+      configDesc.add<std::vector<uint32_t>>("minThreads", desc.minThreads);
+      configDesc.add<std::vector<uint32_t>>("maxThreads", desc.maxThreads);
+      configDesc.add<std::vector<uint32_t>>("minBlocks", desc.minBlocks);
+      configDesc.add<std::vector<uint32_t>>("maxBlocks", desc.maxBlocks);
+      return configDesc;
+    }
+
+    static edm::ParameterSet createDetailedDefaultConfig(const KernelDescription& desc) {
+      edm::ParameterSet pset;
+      pset.addParameter<std::vector<uint32_t>>("threads", desc.threads);
+      pset.addParameter<std::vector<uint32_t>>("blocks", desc.blocks);
+      pset.addParameter<std::vector<uint32_t>>("minThreads", desc.minThreads);
+      pset.addParameter<std::vector<uint32_t>>("maxThreads", desc.maxThreads);
+      pset.addParameter<std::vector<uint32_t>>("minBlocks", desc.minBlocks);
+      pset.addParameter<std::vector<uint32_t>>("maxBlocks", desc.maxBlocks);
+      return pset;
+    }
   };
 
 }  // namespace cms
